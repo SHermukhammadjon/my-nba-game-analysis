147616680e3e04f1fd7ac57deb079e809e693a24
shermuhammad temirov <temirov_s@qwasar.io>
2023-02-20 07:54:59 +0000

    I added print total pts for my_print_nba_stats function

--- my_nba_game_analysis.py
+++ my_nba_game_analysis.py
@@ -37,23 +37,67 @@ def series(series):
         
 
 def print_nba_game_stats(team_dict):
-    for home_or_away, team in team_dict.items():
-        print(f">>> \n{home_or_away}\n")
+    
+    for home_or_away, team_data in team_dict.items():
+        print(f">>> {home_or_away}\n")
         n = 0
-        for team_name, players_data in team.items():
-            n += 1
-            if n != 1:
-                z = 0
-                for player in players_data:
-                    z+=1
-                    if z == 1:
-                        column(player)
+        # print(team_data)
+        total_pts = 0
+        for player in team_data['players_data']:
+            n+=1
+            if n == 1:
+                column(player)
+            series(player)
+            total_pts += player["PTS"]
+        print(f"Tota PTS {total_pts}\n")
+            # print(player)
+        #     print()
+            # n += 1
+            # if n != 1:
+            #     z = 0
+            #     for player in players_data:
+            #         z+=1
+            #         if z == 1:
+            #             column(player)
                         
-                    series(player)
-            else:
-                print(team_name)
-                print(team[team_name])
+            #         series(player)
+            # else:
+            #     print(team_name)
+            #     print(team[team_name])
 
+
+
+# def is_all_num(word):
+#     for n in word:
+#         if n == "a"   
+
+def do_it(nba_data, lable):
+    lable = lable.split(".")
+    equal, key1, do, key2 = lable[0], lable[1], lable[2], lable[3]
+    for home_or_away, team_data in nba_data.items():
+        for player_index in range(len(team_data['players_data'])):
+            if key2.isnumeric():
+                if do == "+":
+                    nba_data[home_or_away]['players_data'][player_index][equal] += nba_data[home_or_away]['players_data'][player_index][key1] + int(key2)
+                elif do == "-":
+                    nba_data[home_or_away]['players_data'][player_index][equal] += nba_data[home_or_away]['players_data'][player_index][key1] - int(key2)
+                elif do == "*":
+                    nba_data[home_or_away]['players_data'][player_index][equal] += nba_data[home_or_away]['players_data'][player_index][key1] * int(key2)
+                elif do == "/":
+                    nba_data[home_or_away]['players_data'][player_index][equal] += nba_data[home_or_away]['players_data'][player_index][key1] / int(key2)
+                # print(nba_data[home_or_away]['players_data'][player_index])
+            else:
+                if do == "+":
+                    nba_data[home_or_away]['players_data'][player_index][equal] += nba_data[home_or_away]['players_data'][player_index][key1] + nba_data[home_or_away]['players_data'][player_index][key2]
+                elif do == "-":
+                    nba_data[home_or_away]['players_data'][player_index][equal] += nba_data[home_or_away]['players_data'][player_index][key1] - nba_data[home_or_away]['players_data'][player_index][key2]
+                elif do == "*":
+                    nba_data[home_or_away]['players_data'][player_index][equal] += nba_data[home_or_away]['players_data'][player_index][key1] * nba_data[home_or_away]['players_data'][player_index][key2]
+                elif do == "/":
+                    nba_data[home_or_away]['players_data'][player_index][equal] += nba_data[home_or_away]['players_data'][player_index][key1] / nba_data[home_or_away]['players_data'][player_index][key2]
+                # print(nba_data[home_or_away]['players_data'][player_index])
+    return nba_data
+          
 
 
 
@@ -66,6 +110,13 @@ def analyse_nba_game(data):
     names = get_names(data.split('\n'))
     respons = separate_team(names, data)
     respons = loader_action(respons, actions)
+    respons = do_it(respons, "PTS.FG.*.2")
+    respons = do_it(respons, "PTS.3P.*.3")
+    respons = do_it(respons, "PTS.FT.+.0")
+    respons = do_it(respons, "TRB.ORB.+.DRB")
+
+    # print_nba_game_stats(respons)
+
 
     return respons
 
